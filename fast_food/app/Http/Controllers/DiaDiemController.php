6d3f25680f26195801accbce3e85b6453b9334db
Dương Nghĩa Hiệp <81470737+dinhthu1405@users.noreply.github.com>
2022-05-26 16:40:10 +0700

    pussh code 26/5

--- fast_food/app/Http/Controllers/DiaDiemController.php
+++ fast_food/app/Http/Controllers/DiaDiemController.php
@@ -111,21 +111,54 @@ class DiaDiemController extends Controller
                 // 'TenDiaDiem.unique' => 'Tên địa điểm đã tồn tại',
             ]
         );
+        // $validator=$request->validate(
+        //     [
+        //         'TenDiaDiem' => 'required',
+        //     ],
+        //     [
+        //         'TenDiaDiem.required' => 'Bạn chưa nhập tên địa điểm',
+        //         // 'TenDiaDiem.unique' => 'Tên địa điểm đã tồn tại',
+        //     ]
+        // );
+        //         $diaDiem = new DiaDiem();
+        // $diaDiem->fill([
+        //     'ten_dia_diem' => $request->TenDiaDiem,
+        //     'thoi_gian_mo' => $request->ThoiGianMo,
+        //     'thoi_gian_dong' => $request->ThoiGianDong,
+        // ]);
+        // // dd($diaDiem);
+        // $ktDiaDiem = DiaDiem::where('ten_dia_diem', $request->input('TenDiaDiem'))->first();
+        // // return ($ktDiaDanh);
+        // if ($ktDiaDiem) {
+        //     return Redirect::back()->with('error', 'Tên địa điểm đã tồn tại');
+        // } else {
+        //     $diaDiem->save(); //lưu xong mới có mã địa điểm
+        //     return Redirect::route('diaDiem.index')->with('success', 'Thêm địa điểm thành công');
+        // }
+        // $diaDiem = new DiaDiem();
+        // $diaDiem->ten_dia_diem = $request->TenDiaDiem;
+        // $diaDiem->thoi_gian_mo = $request->ThoiGianMo;
+        // $diaDiem->thoi_gian_dong = $request->ThoiGianDong;
+
+        // $diaDiem->save();
+        // return Redirect::route('diaDiem.index')->with('success', 'Thêm địa điểm thành công');
+        // return response()->json(['diaDiem'=>$diaDiem]);
         if ($validator->fails()) {
             return response()->json([
                 'status' => 400,
-                'errors' => $validator->errors()->get('*'),
-                // 'error' => $validator->messages()->get('*'),
+                'errors' => $validator->errors(),
+                // 'error' => $validator->messages(),
             ]);
         } else {
             $diaDiem = new DiaDiem();
-            $diaDiem->ten_dia_diem = $request->TenDiaDiem;
-            $diaDiem->thoi_gian_mo = $request->ThoiGianMo;
-            $diaDiem->thoi_gian_dong = $request->ThoiGianDong;
+            $diaDiem->ten_dia_diem = $request->input('ten_dia_diem');
+            $diaDiem->thoi_gian_mo = $request->input('thoi_gian_mo');
+            $diaDiem->thoi_gian_dong = $request->input('thoi_gian_dong');
             $diaDiem->save();
             // return response()->json(['error' => false, 'success' => 'Thêm thành công', 'diaDiem' => $diaDiem], 200);
-            // return response()->json(['status' => 200, 'message' => 'Thêm thành công']);
-            return response()->json(['success' => 'Data is successfully added']);
+            return response()->json(['status' => 200, 'message' => 'Thêm thành công']);
+            // return response()->json(['success' => 'Data is successfully added']);
+
         }
         // return response()->json($diaDiem);
     }
@@ -150,7 +183,21 @@ class DiaDiemController extends Controller
     public function edit(DiaDiem $diaDiem)
     {
         //
-        return view('component/dia-diem/diadiem-edit', compact('diaDiem'));
+if($diaDiem){
+    return response()->json([
+                'status' => 200,
+                'diaDiem' => $diaDiem,
+                // 'error' => $validator->messages()->get('*'),
+            ]);
+}
+else{
+    return response()->json([
+        'status' => 400,
+        'errors' => 'Không tìm thấy địa điểm này',
+        // 'error' => $validator->messages()->get('*'),
+    ]);
+}
+        // return view('component/dia-diem/diadiem-index', compact('diaDiem'));
     }
 
     /**
--- fast_food/resources/views/layouts/app.blade.php
+++ fast_food/resources/views/layouts/app.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-free">
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-free">
 
 <head>
 
     <meta charset="utf-8" />
     <meta name="description" content="" />
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf_token" content="{{ csrf_token() }}" />
     <title>@yield('title')</title>
     <!-- Favicon -->
@@ -15,14 +17,17 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/boxicons.css') }}" />
 
     <!-- Core CSS -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}"
+        class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
 
     <!-- Vendors CSS -->
@@ -36,8 +41,12 @@
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" />
     <!-- <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/page-auth.css') }}" /> -->
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js"
+        integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- Helpers -->
     <script src="{{ asset('assets/vendor/js/helpers.js') }}"></script>
 
@@ -49,16 +58,16 @@
 
 <body>
     @if ($pageId == 'login' || $pageId == 'register')
-    @yield('content')
+        @yield('content')
     @else
-    @include('pages/header')
-    @yield('content')
-    @include('pages/footer')
+        @include('pages/header')
+        @yield('content')
+        @include('pages/footer')
     @endif
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
 
-    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    {{-- <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script> --}}
     <script src=" {{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
     <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
