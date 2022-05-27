69df4b13bf189e81b2c4a28ffa373470d5f0d55e
Dương Nghĩa Hiệp <81470737+dinhthu1405@users.noreply.github.com>
2022-05-27 16:51:53 +0700

    push code (27-5-2022)

--- fast_food/resources/views/component/trang-thai-don-hang/trangthaidonhang-show.blade.php
+++ fast_food/resources/views/component/trang-thai-don-hang/trangthaidonhang-show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app', ['pageId' => ''])
 
-@section('title', 'Trang quản lí đơn hàng')
+@section('title', 'Trang quản lí trạng thái đơn hàng')
 @section('content')
     <div class="content-wrapper">
         <!-- Content -->
@@ -10,10 +10,11 @@
                 <div class="col-md-6">
                     <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Basic Tables</h4>
                 </div>
-                <div class="col-md-4"></div>
-                <div class="col-md-2">
-                    <a href="{{ route('monAn.create') }}"><button type="button" class="btn btn-success py-2 mb-4">Thêm
-                            đơn hàng</button></a>
+                <div class="col-md-3"></div>
+                <div class="col-md-3">
+                    <a href="{{ route('trangThaiDonHang.create') }}"><button type="button"
+                            class="btn btn-success py-2 mb-4">Thêm
+                            Trạng Thái Đơn Hàng</button></a>
                 </div>
             </div>
             <form action="{{ route('monAn.search') }}" method="post">
@@ -31,41 +32,32 @@
             <br />
             <!-- Bootstrap Table with Header - Light -->
             <div class="card">
-                <h5 class="card-header">Danh sách đơn hàng</h5>
+                <h5 class="card-header">Danh sách trạng thái đơn hàng</h5>
                 <div class="table-responsive text-nowrap">
                     <table class="table">
                         <thead class="table-light">
                             <tr>
                                 <th>STT</th>
-                                <th>Ngày lập đơn hàng</th>
-                                <th>Tổng tiền</th>
-                                <th>Trạng thái đơn hàng</th>
-                                <th>Người giao hàng</th>
-                                <th>Người đặt</th>
-                                <th>Tình trạng</th>
+                                <th>Tên trạng thái</th>
                                 <th>Chỉnh sửa</th>
                                 <th>Xoá</th>
                             </tr>
                         </thead>
                         <?php $count = 1; ?>
-                        @foreach ($lstDonHang as $donHang)
+                        @foreach ($lstTrangThaiDonHang as $trangThaiDonHang)
                             <tbody class="table-border-bottom-0">
                                 <tr>
                                     <td> {{ $count++ }} </td>
                                     <td><i class="fab fa-angular fa-lg text-danger me-3"></i>
-                                        <strong>{{ $donHang->ngay_lap_dh }}</strong>
+                                        <strong>{{ $trangThaiDonHang->ten_trang_thai }}</strong>
                                     </td>
-                                    <td>{{ $donHang->tong_tien }}</td>
-                                    <td>{{ $donHang->trangThaiDonHang->ten_trang_thai }}</td>
-                                    <td>{{ $donHang->so_luong }}</td>
-                                    <td>{{ $donHang->nguoi_giao_hang_id }}</td>
-                                    <td>{{ $donHang->user_id }}</td>
-                                    <td><a href="{{ route('donHang.edit', $donHang->id) }}"><button type="button"
-                                                id="btn-edit" class="btn btn-warning py-2 mb-4" data-target="#modal-edit"
-                                                data-bs-toggle="modal" data-bs-target="#modalCenter-Edit">
+                                    <td><a href="{{ route('trangThaiDonHang.edit', $trangThaiDonHang->id) }}"><button
+                                                type="button" id="btn-edit" class="btn btn-warning py-2 mb-4"
+                                                data-target="#modal-edit" data-bs-toggle="modal"
+                                                data-bs-target="#modalCenter-Edit">
                                                 <i class="bx bx-edit-alt me-1"></i> </button></a> </td>
-                                    <td> <a href="{{ route('donHang.xoa', $donHang->id) }}"
-                                            onclick="return confirm('Bạn có chắc muốn xoá món ăn này')"><button
+                                    <td> <a href="{{ route('trangThaiDonHang.xoa', $trangThaiDonHang->id) }}"
+                                            onclick="return confirm('Bạn có chắc muốn xoá trạng thái đơn hàng này')"><button
                                                 type="button" id="btn-edit" class="btn btn-danger py-2 mb-4"
                                                 data-target="#modal-edit" data-bs-toggle="modal"
                                                 data-bs-target="#modalCenter-Edit">
