a282338b50f1549ce940f0ec6c8d43f8119ccdc7
ThuTran <0306191085@caothang.edu.vn>
2022-07-19 23:13:41 +0700

    chuyển file

--- fast_food/resources/views/Partial/danh-gia/JSPartial-danhgia-show.blade.php
+++
@@ -1,9 +0,0 @@
-<script>
-    $(function() {
-        $('#refresh').click(function() {
-            var search = "";
-            document.getElementById("timKiem").value = search;
-            console.log(search);
-        });
-    });
-</script>
